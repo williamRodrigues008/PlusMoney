69e69f4bd4421b67848490cae80d8a5a8d1d7c7c
William Rodrigues da Silva <wrsilva008@gmail.com>
2024-07-19 10:58:43 -0300

    [Development] - "Criação da classe Movimentação e contexto banco de dados"

---
+++ PlusMoney.API/Models/DbContexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace PlusMoney.API.Models
+{
+    public class DbContexto : DbContext
+    {
+        public DbContexto(DbContextOptions<DbContexto> options) : base(options) { }
+
+        public DbSet<Movimentacao> Movimentacao { get; set; }
+    }
+}
---
+++ PlusMoney.API/Models/Movimentacao.cs
@@ -0,0 +1,16 @@
+﻿namespace PlusMoney.API.Models
+{
+    public class Movimentacao
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; } = string.Empty;
+        public string TipoMovimentacao { get; set; } = string.Empty;
+        public decimal Valor { get; set; }
+
+
+        public Movimentacao()
+        {
+
+        }
+    }
+}

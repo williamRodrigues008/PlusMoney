5bab0b5e18295f37274c04320134977310a3e198
William Rodrigues da Silva <wrsilva008@gmail.com>
2024-07-19 10:57:07 -0300

    [Development] - "Recuperação de dados de movimentação para o front"

--- PlusMoney/Program.cs
+++ PlusMoney/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents();
+builder.Services.AddSingleton<HttpClient>();
 
 var app = builder.Build();
 

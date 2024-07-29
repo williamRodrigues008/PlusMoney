79bb2d6a7f7ba5515c5ccd2b49b4df650e90c8ed
William Rodrigues da Silva <wrsilva008@gmail.com>
2024-07-28 23:16:20 -0300

    [Development] - "Adição de componetes RadzenBlazor"

--- PlusMoney/Program.cs
+++ PlusMoney/Program.cs
@@ -1,9 +1,11 @@
 using PlusMoney.Components;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 builder.Services.AddSingleton<HttpClient>();
 
 var app = builder.Build();
@@ -21,6 +23,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();

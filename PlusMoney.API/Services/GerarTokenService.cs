b6bde0b2bb620e0c54857cec1956adbc98400dad
William Rodrigues da Silva <wrsilva008@gmail.com>
2024-12-20 14:23:11 -0300

    [Development] - "Criação de services da API"

---
+++ PlusMoney.API/Services/GerarTokenService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using PlusMoney.API.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PlusMoney.API.Services
+{
+    public class GerarTokenService
+    {
+        public dynamic GerarToken(Usuario usuario)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("Nome", usuario.NomeCompleto!),
+                new Claim("Usuario", usuario.NomeUsuario!),
+                new Claim("Email", usuario.Email!),
+                new Claim("Id", usuario.Id.ToString())
+            };
+
+            var expiracao = DateTime.Now.AddDays(1);
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("key-plus-money-access-application"));
+            var dadosToken = new JwtSecurityToken(
+                    signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256),
+                    claims: claims,
+                    expires: expiracao
+                );
+
+            var token = new JwtSecurityTokenHandler().WriteToken(dadosToken);
+
+            return new
+            {
+                access_token = token,
+                expires = expiracao
+            };
+        }
+    }
+}
--- PlusMoney.API/Services/MovimentacaoService.cs
+++ PlusMoney.API/Services/MovimentacaoService.cs
@@ -3,13 +3,25 @@ using PlusMoney.API.Models;
 
 namespace PlusMoney.API.Services
 {
-    public class MovimentacaoService : IMovimentacao
+    public class MovimentacaoService : ILeituraEscritaMovimentacao
     {
         private readonly DbContexto _contextoDb;
 
         public MovimentacaoService(DbContexto contextoDb)
         {
             _contextoDb = contextoDb;
+        }
+
+        public string AdicionarMovimentacao(Movimentacao movimentacao)
+        {
+            if(movimentacao != null)
+            {
+                _contextoDb.Add(movimentacao);
+                _contextoDb.SaveChanges();
+                return "Sucesso ao adicionar nova movimentação!";
+
+            }
+            return "Ops, houve um erro na criação da nova movimentação";
         }
 
         public async Task<IEnumerable<Movimentacao>> ListarMovimentacao()
